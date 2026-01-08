e7089827ab7064143f3b97d721e55690422cd1b3
gentle2023 <iani@byupathway.edu>
2026-01-08 01:35:21 +0100

    commit

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+        Console.WriteLine();
+
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
 }
+
+
