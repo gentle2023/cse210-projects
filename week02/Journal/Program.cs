bf9c475f74bae844101bd3674f458e9af9cbbf4a
praizarts <mbachuignatia@gmail.com>
2026-01-19 00:20:08 +0100

    Week 2 Journal

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,5 +5,62 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+
+        Journal theJournal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        bool quit = false;
+
+        while (!quit)
+        {
+            Console.WriteLine("\n1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Quit");
+            Console.Write("Choose an option: ");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine(prompt);
+                Console.Write("> ");
+                string response = Console.ReadLine();
+
+                Console.Write("How are you feeling today? ");
+                string mood = Console.ReadLine();
+
+                Entry entry = new Entry();
+                entry._date = DateTime.Now.ToShortDateString();
+                entry._promptText = prompt;
+                entry._entryText = response;
+                entry._mood = mood;
+
+                theJournal.AddEntry(entry);
+            }
+            else if (choice == "2")
+            {
+                theJournal.DisplayAll();
+            }
+            else if (choice == "3")
+            {
+                Console.Write("Filename: ");
+                string file = Console.ReadLine();
+                theJournal.SaveToFile(file);
+            }
+            else if (choice == "4")
+            {
+                Console.Write("Filename: ");
+                string file = Console.ReadLine();
+                theJournal.LoadFromFile(file);
+            }
+            else if (choice == "5")
+            {
+                quit = true;
+            }
+        }
+
     }
 }
---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+public class PromptGenerator
+{
+    // public List<string> _prompts;
+    private List<string> _prompts = new List<string>
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?",
+        "What is my schedule for today?",
+        ""
+    };
+    public string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+}
