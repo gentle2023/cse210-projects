cf62b1197efbf7a50b2af29b8c78f57ae75557cb
gentle2023 <iani@byupathway.edu>
2026-02-17 03:00:34 +0100

    week07: final project

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Console.WriteLine();
+
+        Running run = new Running(new DateTime(2022, 11, 3), 30, 3.0);
+        Cycling bike = new Cycling(new DateTime(2022, 11, 3), 45, 12.0);
+        Swimming swim = new Swimming(new DateTime(2022, 11, 3), 20, 20);
+
+        List<Activity> activities = new List<Activity>() { run, bike, swim };
+
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
